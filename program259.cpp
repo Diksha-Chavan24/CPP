f583fa8f4ed0bca18d32c7f33e324826142c88a3
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-30 17:03:49 +0000

    Add files via upload

---
+++ program259.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void Display(float Arr[], int Length)
+{
+    int iCnt;
+    for(iCnt = 0; iCnt < Length; iCnt++)
+    {
+        cout<<Arr[iCnt]<<"\n";
+    }
+}
+
+int main()
+{
+    int Size, iCnt;
+
+    cout<<"Enter the number of elements: \n";
+    cin>>Size;
+
+    float *ptr = new float[Size];
+
+    cout<<"Enter the elements :\n";
+
+    for(iCnt = 0; iCnt < Size; iCnt++)
+    {
+        cin>>ptr[iCnt];
+    }
+
+    cout<<"Elements are : \n";
+
+    Display(ptr, Size);
+
+    delete []ptr;
+
+    return 0;
+}
+
+
+// accept n numbers from user and return largest value
