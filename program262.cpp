aca6a124ae1ae5ad8f8583c881e6b2c332b6cb97
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-31 16:36:11 +0000

    Add files via upload

---
+++ program262.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+T Addition(T Arr[], int Length)
+{
+    int iCnt = 0;
+    T Sum = 0;
+
+    for(iCnt = 0; iCnt < Length; iCnt++)
+    {
+        Sum = Sum + Arr[iCnt];
+    }
+    return Sum;
+}
+
+int main()
+{
+    int Size, iCnt = 0;
+    float Ret = 0.0f;
+
+    cout<<"Enter the number of elements: \n";
+    cin>>Size;
+
+    float *ptr = new float[Size];
+
+    cout<<"Enter the elements :\n";
+
+    for(iCnt = 0; iCnt < Size; iCnt++)
+    {
+        cin>>ptr[iCnt];
+    }
+
+    cout<<"Elements are : \n";
+
+    Ret = Addition(ptr, Size);
+
+    cout<<"Addition is : "<<Ret<<"\n";
+
+    delete []ptr;
+
+    return 0;
+}
+
+
+// accept n numbers from user and return largest value
