b377351ead22611276128960ac4899234db175ae
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-31 16:43:07 +0000

    Add files via upload

---
+++ program266.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int CountOne(int iNo )
+{
+    int iDigit = 0, iCount = 0; 
+
+    while(iNo !=0)
+    {
+        iDigit = iNo % 2;
+        iCount = iCount + iDigit;
+        iNo = iNo / 2;   
+    }
+
+    return iCount;
+}
+
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    cout<<"Enter the number : \n";
+    cin>>iValue;
+    
+    iRet = CountOne(iValue);
+
+    cout<<"Number of 1 are : "<<iRet<<"\n";
+
+    return 0;
+}
+
