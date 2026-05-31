dc68b77240590c750dc19943748dbc2ce43a8fd1
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-31 16:46:47 +0000

    Add files via upload

---
+++ program269.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    cout<<"Number in Decimal format : "<<std::dec<<iValue<<"\n"; 
+    cout<<"Number in Octal format : "<<std::oct<<iValue<<"\n"; 
+    cout<<"Number in Hexdecimal format : "<<std::hex<<iValue<<"\n"; 
+
+    return 0;
+}
+
