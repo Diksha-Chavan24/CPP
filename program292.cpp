1c4894cdca1e17140b16b0c384c0093649bf284b
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-06-15 14:03:53 +0000

    Add files via upload

---
+++ program292.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+typedef unsigned int UINT; //12th position
+
+UINT ChangeBit(UINT iNo)
+{
+    UINT iMask = 0xfffff7ff;
+    UINT iResult = 0;
+
+    iResult = iNo & iMask;
+
+    return iResult;
+
+}
+
+int main()
+{
+    UINT iValue = 0, iRet = 0 ; 
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    iRet = ChangeBit(iValue);
+
+    cout<<"Updated number is "<<iRet;
+
+    return 0;
+}
