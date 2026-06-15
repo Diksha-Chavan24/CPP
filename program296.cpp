b6b235a65c5b8e8a1a6284f3e3e09eb5d32aac22
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-06-15 14:07:03 +0000

    Add files via upload

---
+++ program296.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+//Call by address
+void Swap(int *No1, int *No2) 
+{
+    int Temp = 0;
+
+    Temp = *No1;
+    *No1 = *No2;
+    *No2 = Temp;
+}
+
+int main()
+{
+    int a = 10, b = 11, iRet = 0;
+    
+    Swap(&a,&b);
+
+    cout<<"Value of a :"<<a<<"\n";
+    cout<<"Value of b :"<<b<<"\n";
+
+    return 0;
+}
